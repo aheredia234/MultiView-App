e403a748c852a024bedc12e463f0b01ca13e1cce
Angie <42586731+aheredia234@users.noreply.github.com>
2020-02-26 15:58:45 -0600

    Add files via upload

---
+++ View Switcher/View Switcher/BlueViewController.swift
@@ -0,0 +1,40 @@
+//
+//  BlueViewController.swift
+//  View Switcher
+//
+//  Created by Angela Heredia on 2/26/20.
+//  Copyright © 2020 Angela Heredia. All rights reserved.
+//
+
+import UIKit
+
+class BlueViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    @IBAction func blueButtonPressed(sender: UIButton)
+    {
+        let alert = UIAlertController(title: "Blue View Button Pressed", message: "You pressed the button on the blue view", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Yes, I did", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+    }
+
+}
---
+++ View Switcher/View Switcher/YellowViewController.swift
@@ -0,0 +1,38 @@
+//
+//  YellowViewController.swift
+//  View Switcher
+//
+//  Created by Angela Heredia on 2/26/20.
+//  Copyright © 2020 Angela Heredia. All rights reserved.
+//
+
+import UIKit
+
+class YellowViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func yellowButtonPressed(sender: UIButton)
+    {
+        let alert = UIAlertController(title: "Yellow View Button Pressed", message: "You pressed the button on the yellow view", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Yes, I did", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+}
